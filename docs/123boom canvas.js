2c7320e85ee19814db753ae2c12450030c50b20c
melusc <lusc1mc@gmail.com>
2020-09-10 21:11:14 +0200

    Reformating

--- docs/123boom canvas.js
+++ docs/123boom canvas.js
@@ -1,117 +1,120 @@
+'use strict';
+
 {
-  const canvas = document.getElementById('game-area');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById( 'game-area' );
+  const ctx = canvas.getContext( '2d' );
   const color = 'white';
   let width;
   let height;
 
   let number = 1;
 
-  let record = localStorage.getItem('record') ?? 1;
+  let record = localStorage.getItem( 'record' ) ?? 1;
 
   let failedScreen = false;
 
   const update = () => {
     /* Clear only text */
-    ctx.clearRect(width * 0.42 + 2, 0, width * 0.16 - 4, height);
+    ctx.clearRect( ( width * 0.42 ) + 2, 0, ( width * 0.16 ) - 4, height );
 
     /* Current number */
     let fontSize = 60;
-    ctx.font = `bold ${fontSize}px Quicksand`;
+    ctx.font = `bold ${ fontSize }px Quicksand`;
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillStyle = color;
 
     const availWidth = width * 0.16 * 0.9;
-    while (ctx.measureText(number).width > availWidth) {
+    while ( ctx.measureText( number ).width > availWidth ) {
       fontSize--;
-      ctx.font = `bold ${fontSize}px Quicksand`;
+      ctx.font = `bold ${ fontSize }px Quicksand`;
     }
 
-    ctx.measureText(number);
-    ctx.fillText(number, width / 2, (height + 60) / 2);
+    ctx.measureText( number );
+    ctx.fillText( number, width / 2, ( height + 60 ) / 2 );
 
     /* Record */
     ctx.textBaseline = 'top';
     ctx.font = 'bold 30px Quicksand';
-    ctx.fillText('Current best:', width / 2, height * 0.05);
+    ctx.fillText( 'Current best:', width / 2, height * 0.05 );
 
     ctx.font = ' bold 30px Quicksand';
-    ctx.fillText(record, width / 2, height * 0.05 + 30);
+    ctx.fillText( record, width / 2,( height * 0.05 ) + 30 );
   };
 
   const init = () => {
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect( 0, 0, width, height );
 
     /* Left */
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = 2;
-    ctx.rect(width * 0.02, height * 0.05 + 60, width * 0.4, height * 0.9 - 60);
+    ctx.rect( width * 0.02, ( height * 0.05 ) + 60, width * 0.4, ( height * 0.9 ) - 60 );
     ctx.stroke();
     ctx.closePath();
 
     /* Text */
     let fontSize = 60;
-    ctx.font = `${fontSize}px Quicksand`;
+    ctx.font = `${ fontSize }px Quicksand`;
 
     const availWidth = width * 0.4 * 0.9;
 
     while (
-      Math.max(ctx.measureText('Boom').width, ctx.measureText('Safe').width) >
-      availWidth
+      Math.max( ctx.measureText( 'Boom' ).width, ctx.measureText( 'Safe' ).width )
+      > availWidth
     ) {
       fontSize--;
-      ctx.font = `${fontSize}px Quicksand`;
+      ctx.font = `${ fontSize }px Quicksand`;
     }
 
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillStyle = color;
-    ctx.fillText('Boom', width * 0.22, (height + 60) / 2);
+    ctx.fillText( 'Boom', width * 0.22, ( height + 60 ) / 2 );
 
     /* Right */
     ctx.beginPath();
 
     /* Same vals as above */
-    ctx.rect(width * 0.58, height * 0.05 + 60, width * 0.4, height * 0.9 - 60);
+    ctx.rect( width * 0.58, ( height * 0.05 ) + 60, width * 0.4, ( height * 0.9 ) - 60 );
     ctx.stroke();
     ctx.closePath();
 
     /* Text */
     /* Same vals as above */
-    ctx.fillText('Safe', width * 0.78, (height + 60) / 2);
+    ctx.fillText( 'Safe', width * 0.78, ( height + 60 ) / 2 );
 
     update();
   };
 
-  addEventListener('resize', () => {
+  addEventListener( 'resize', () => {
     canvas.width = width = innerWidth;
     canvas.height = height = innerHeight;
 
-    if (failedScreen === false) {
+    if ( failedScreen === false ) {
       init();
-    } else if (failedScreen === true) {
+    }
+    else if ( failedScreen === true ) {
       fail();
     }
-  });
+  } );
 
   const fail = () => {
-    if (number > record) {
+    if ( number > record ) {
       record = number;
 
-      localStorage.setItem('record', record);
+      localStorage.setItem( 'record', record );
     }
 
     failedScreen = true;
 
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect( 0, 0, width, height );
 
     /* Box */
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = 2;
-    ctx.rect(width * 0.15, height * 0.2, width * 0.7, height * 0.6);
+    ctx.rect( width * 0.15, height * 0.2, width * 0.7, height * 0.6 );
     ctx.stroke();
     ctx.closePath();
 
@@ -120,65 +123,65 @@
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillStyle = color;
-    ctx.fillText('Restart?', width / 2, height / 2);
+    ctx.fillText( 'Restart?', width / 2, height / 2 );
   };
 
-  canvas.addEventListener('click', e => {
+  canvas.addEventListener( 'click', e => {
     const rect = canvas.getBoundingClientRect();
 
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    if (failedScreen === false) {
+    if ( failedScreen === false ) {
       const isBoom = number % 7 === 0 || number % 3 === 0;
 
       if (
-        x >= width * 0.02 &&
-        x <= width * 0.42 &&
-        y >= height * 0.05 + 60 &&
-        y <= height * 0.95
+        x >= width * 0.02
+        && x <= width * 0.42
+        && y >= ( height * 0.05 ) + 60
+        && y <= height * 0.95
       ) {
-        if (isBoom) {
+        if ( isBoom ) {
           number++;
           update();
-        } else {
-          fail();
         }
-      } else if (
-        x >= width * 0.58 &&
-        x <= width * 0.98 &&
-        y >= height * 0.05 + 60 &&
-        y <= height * 0.95
-      ) {
-        if (!isBoom) {
-          number++;
-          update();
-        } else {
+        else {
           fail();
         }
       }
-    } else {
+      else if (
+        x >= width * 0.58
+        && x <= width * 0.98
+        && y >= ( height * 0.05 ) + 60
+        && y <= height * 0.95
+      ) {
+        ( isBoom
+          ? fail
+          : ( number++, update ) )();
+      }
+    }
+    else {
       number = 1;
       failedScreen = false;
       init();
     }
-  });
+  } );
 
   canvas.width = width = innerWidth;
   canvas.height = height = innerHeight;
 
-  new FontFace('Quicksand', 'url(fonts/Quicksand-VariableFont_wght.ttf)', {
+  new FontFace( 'Quicksand', 'url(fonts/Quicksand-VariableFont_wght.ttf)', {
     family: 'Quicksand',
     style: 'normal',
     weight: '200',
-  })
+  } )
     .load()
-    .then(font_quicksand => {
-      document.fonts.add(font_quicksand);
+    .then( fontQuicksand => {
+      document.fonts.add( fontQuicksand );
       init();
-    });
+    } );
 
-  canvas.addEventListener('mousemove', e => {
+  canvas.addEventListener( 'mousemove', e => {
     const rect = canvas.getBoundingClientRect();
 
     const x = e.clientX - rect.left;
@@ -186,31 +189,32 @@
 
     if (
       /* Left button */
-      (failedScreen === false &&
-        x >= width * 0.02 &&
-        x <= width * 0.42 &&
-        y >= height * 0.05 + 60 &&
-        y <= height * 0.95) ||
+      ( failedScreen === false
+        && x >= width * 0.02
+        && x <= width * 0.42
+        && y >= ( height * 0.05 ) + 60
+        && y <= height * 0.95 )
       /* Right button */
-      (failedScreen === false &&
-        x >= width * 0.58 &&
-        x <= width * 0.98 &&
-        y >= height * 0.05 + 60 &&
-        y <= height * 0.95) ||
+      || ( failedScreen === false
+        && x >= width * 0.58
+        && x <= width * 0.98
+        && y >= ( height * 0.05 ) + 60
+        && y <= height * 0.95 )
       /* Restart button */
-      (failedScreen === true &&
-        x >= width * 0.15 &&
-        x <= width * 0.85 &&
-        y >= height * 0.2 &&
-        y <= height * 0.8)
+      || ( failedScreen === true
+        && x >= width * 0.15
+        && x <= width * 0.85
+        && y >= height * 0.2
+        && y <= height * 0.8 )
     ) {
-      canvas.classList.add('pointer');
-    } else {
+      canvas.classList.add( 'pointer' );
+    }
+    else {
       removePointer();
     }
-  });
+  } );
 
-  const removePointer = () => void canvas.classList.remove('pointer');
-  canvas.addEventListener('blur', removePointer);
-  canvas.addEventListener('mouseleave', removePointer);
+  const removePointer = () => void canvas.classList.remove( 'pointer' );
+  canvas.addEventListener( 'blur', removePointer );
+  canvas.addEventListener( 'mouseleave', removePointer );
 }
--- docs/time to summer holidays rewrite.js
+++ docs/time to summer holidays rewrite.js
@@ -1,66 +1,72 @@
+'use strict';
+
 {
-  const summerDate = new Date('07/08/2021 14:40:00 GMT+0200'); // MM/DD/YYYY ...
+  const summerDate = new Date( '07/08/2021 14:40:00 GMT+0200' ); // MM/DD/YYYY ...
 
   const setTime = () => {
-    const totalSeconds = (summerDate.getTime() - Date.now()) / 1000;
+    const totalSeconds = ( summerDate.getTime() - Date.now() ) / 1000;
 
-    const days = Math.floor(totalSeconds / (60 * 60 * 24));
-    const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-    const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-    const seconds = Math.floor(totalSeconds % 60);
+    const days = Math.floor( totalSeconds / ( 60 * 60 * 24 ) );
+    const hours = Math.floor( ( totalSeconds % ( 60 * 60 * 24 ) ) / ( 60 * 60 ) );
+    const minutes = Math.floor( ( totalSeconds % ( 60 * 60 ) ) / 60 );
+    const seconds = Math.floor( totalSeconds % 60 );
 
-    setText({
+    setText( {
       id: 'days',
       val: days,
       text: 'day',
       leadingZero: false,
-    });
-    setText({
+    } );
+    setText( {
       id: 'hours',
       val: hours,
       text: 'hour',
-    });
-    setText({
+    } );
+    setText( {
       id: 'minutes',
       val: minutes,
       text: 'minute',
-    });
-    setText({
+    } );
+    setText( {
       id: 'seconds',
       val: seconds,
       text: 'second',
-    });
+    } );
 
     const dateZeroMilliSeconds = new Date();
-    dateZeroMilliSeconds.setSeconds(dateZeroMilliSeconds.getSeconds() + 1, 0);
-    setTimeout(() => {
+    dateZeroMilliSeconds.setSeconds( dateZeroMilliSeconds.getSeconds() + 1, 0 );
+    setTimeout( () => {
       setTime();
-    }, dateZeroMilliSeconds.getTime() - Date.now());
+    }, dateZeroMilliSeconds.getTime() - Date.now() );
   };
 
-  const setText = ({ id, val, text, leadingZero }) => {
-    leadingZero = leadingZero ?? true;
+  const setText = ( { id, val, text, leadingZero } ) => {
+    const el = document.getElementById( id );
+    const string = leadingZero ?? true
+      ? `0${ val }`.slice( -2 )
+      : `${ val }`;
 
-    const el = document.getElementById(id);
-    const string = leadingZero ? ('0' + val).slice(-2) : '' + val;
-
-    if (el.textContent !== string) {
+    if ( el.textContent !== string ) {
       el.textContent = string;
 
-      const display = text + (val === 1 ? '' : 's');
-      const displayEl = document.getElementById(id + 'Display');
+      const display = text + ( val === 1
+        ? ''
+        : 's' );
+      const displayEl = document.getElementById( `${ id }Display` );
 
-      displayEl.textContent = display === false ? '' : display;
+      displayEl.textContent = display === false
+        ? ''
+        : display;
     }
   };
 
   setTime();
 
   const dateZeroMilliSeconds = new Date();
-  dateZeroMilliSeconds.setSeconds(dateZeroMilliSeconds.getSeconds() + 1, 0);
-  // run always at 0 ms ( is off by a bit though due to slight inaccuracies )
+  dateZeroMilliSeconds.setSeconds( dateZeroMilliSeconds.getSeconds() + 1, 0 );
+  // Run always at 0 ms ( is off by a bit though due to slight inaccuracies )
 
-  setTimeout(() => {
+  setTimeout( () => {
     setTime();
-  }, dateZeroMilliSeconds.getTime() - Date.now());
+  }, dateZeroMilliSeconds.getTime() - Date.now() );
 }
