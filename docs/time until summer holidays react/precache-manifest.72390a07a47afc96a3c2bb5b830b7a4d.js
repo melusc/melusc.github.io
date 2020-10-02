56cdfb61a00f0d5ee99d73a241f276dfe91c7375
melusc <lusc1mc@gmail.com>
2020-10-02 11:39:59 +0200

    Rebuilt it with proper "homepage"

---
+++ docs/time until summer holidays react/precache-manifest.72390a07a47afc96a3c2bb5b830b7a4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e339b1d42efdad482c4621a0953bd0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "681eec802c83dd8609fd",
+    "url": "./static/css/main.ea95ee09.chunk.css"
+  },
+  {
+    "revision": "87e9010209948b6fc41e",
+    "url": "./static/js/2.3ab13534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3ab13534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681eec802c83dd8609fd",
+    "url": "./static/js/main.ba44e6f2.chunk.js"
+  },
+  {
+    "revision": "b793c38fc953fa14b65d",
+    "url": "./static/js/runtime-main.208b55f4.js"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "./static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  }
+]);
--- docs/time until summer holidays react/service-worker.js
+++ docs/time until summer holidays react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dac7251fb7b9f24d4465026018214477.js"
+  "./precache-manifest.72390a07a47afc96a3c2bb5b830b7a4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
