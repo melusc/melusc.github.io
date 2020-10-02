a1281e9ceb878dd61e7c541eeae19124a1a0f465
melusc <lusc1mc@gmail.com>
2020-10-02 11:32:33 +0200

    First react app :fire:

---
+++ docs/time until summer holidays react/precache-manifest.dac7251fb7b9f24d4465026018214477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e9c54819832bc73554c92c5f5f1415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4b18cf77cda0dcd4c7",
+    "url": "/static/css/main.8a5e7561.chunk.css"
+  },
+  {
+    "revision": "87e9010209948b6fc41e",
+    "url": "/static/js/2.3ab13534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ab13534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4b18cf77cda0dcd4c7",
+    "url": "/static/js/main.ba44e6f2.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  }
+]);
