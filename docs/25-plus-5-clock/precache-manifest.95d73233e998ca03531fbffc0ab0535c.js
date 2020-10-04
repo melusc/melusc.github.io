2918729c191351045b83ce28250e730d5e825974
melusc <lusc1mc@gmail.com>
2020-10-04 18:11:47 +0200

    FreeCodeCamp 25 + 5 clock challenge

---
+++ docs/25-plus-5-clock/precache-manifest.95d73233e998ca03531fbffc0ab0535c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2005ed05bc953b0a65b4b1db8dc24c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "363545a05eab89bba5d1",
+    "url": "./static/css/main.c534cf12.chunk.css"
+  },
+  {
+    "revision": "49119a26075db9847d6c",
+    "url": "./static/js/2.575583fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.575583fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363545a05eab89bba5d1",
+    "url": "./static/js/main.2f59a5ec.chunk.js"
+  },
+  {
+    "revision": "0f17d4d6eb3b56000877",
+    "url": "./static/js/runtime-main.728e44db.js"
+  }
+]);
