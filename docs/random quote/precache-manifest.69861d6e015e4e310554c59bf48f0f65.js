8fcd7558cc8b06f18885ff13e97c4cc63e1eba5f
melusc <lusc1mc@gmail.com>
2020-10-05 16:03:58 +0200

    Yet another freecodecamp challenge

---
+++ docs/random quote/precache-manifest.69861d6e015e4e310554c59bf48f0f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d400324267668b24d2b1d0cfcd063a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d29a6037a0cf207dd8b",
+    "url": "./static/css/main.c19a5be5.chunk.css"
+  },
+  {
+    "revision": "cf49a7d1e3e78312fc50",
+    "url": "./static/js/2.95e4e213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.95e4e213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d29a6037a0cf207dd8b",
+    "url": "./static/js/main.f487e662.chunk.js"
+  },
+  {
+    "revision": "8314e3554cb354590f57",
+    "url": "./static/js/runtime-main.05d1baf0.js"
+  }
+]);
