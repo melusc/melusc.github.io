253e573541b21fccac50a4037aa50a73ba51865b
Luca <61409445+melusc@users.noreply.github.com>
2021-12-01 16:51:52 +0100

    Add operations

---
+++ test/truth-table/lib/operations.test.ts
@@ -0,0 +1,59 @@
+import test from 'ava';
+
+import {operations} from '../../../src/truth-table/lib/operations';
+
+const createTest = <T extends keyof typeof operations>(
+	title: T,
+	valueResults: Array<[Parameters<typeof operations[T]>, boolean]>,
+) => {
+	test(title, t => {
+		for (const row of valueResults) {
+			t.is(
+				// @ts-expect-error: row[0] is a tuple
+				operations[title](...row[0]),
+				row[1],
+				`${title}(${row[0].join(', ')}) should equal ${String(row[1])}`,
+			);
+		}
+	});
+};
+
+createTest('iff', [
+	[[true, true], true],
+	[[true, false], false],
+	[[false, true], false],
+	[[false, false], true],
+]);
+
+createTest('if-then', [
+	[[true, true], true],
+	[[true, false], false],
+	[[false, true], true],
+	[[false, false], true],
+]);
+
+createTest('not', [
+	[[true], false],
+	[[false], true],
+]);
+
+createTest('and', [
+	[[true, true], true],
+	[[true, false], false],
+	[[false, true], false],
+	[[false, false], false],
+]);
+
+createTest('xor', [
+	[[true, true], false],
+	[[true, false], true],
+	[[false, true], true],
+	[[false, false], false],
+]);
+
+createTest('or', [
+	[[true, true], true],
+	[[true, false], true],
+	[[false, true], true],
+	[[false, false], false],
+]);
