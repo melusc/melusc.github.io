aab80f1916dc0cdd5438f724cf8ae49f061ae154
Luca <61409445+melusc@users.noreply.github.com>
2021-12-02 19:06:19 +0100

    Add eval

---
+++ src/truth-table/lib/eval.ts
@@ -0,0 +1,41 @@
+import {type Operation} from './parse-operation';
+import {operations} from './operations';
+import {operationToString} from './operation-to-string';
+
+export const evalOperation = (
+	operation: Operation,
+	variables: Record<string, boolean>,
+): boolean => {
+	const stringified = operationToString(operation);
+
+	const cached = variables[stringified];
+
+	if (cached) {
+		return cached;
+	}
+
+	let result: boolean;
+
+	switch (operation.type) {
+		case 'variable': {
+			result = variables[operation.variable]!;
+			break;
+		}
+
+		case 'not': {
+			result = operations.not(evalOperation(operation.values[0], variables));
+			break;
+		}
+
+		default: {
+			result = operations[operation.type](
+				evalOperation(operation.values[0], variables),
+				evalOperation(operation.values[1], variables),
+			);
+			break;
+		}
+	}
+
+	variables[stringified] = result;
+	return result;
+};
