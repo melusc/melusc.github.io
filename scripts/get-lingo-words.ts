513ecd0028c0d69b8666adcf239b761c79c70c1e
Luca <61409445+melusc@users.noreply.github.com>
2021-12-31 17:45:57 +0100

    Emit commonjs for scripts (ava requires package.json can't have type: module)

--- scripts/get-lingo-words.ts
+++ scripts/get-lingo-words.ts
@@ -1,47 +1,61 @@
 import fs from 'node:fs/promises';
 import https from 'node:https';
 import {Buffer} from 'node:buffer';
+import path from 'node:path';
 
 const filenames = ['jargon', 'science-terms', 'wordlist'];
 
-const get = async (url: URL): Promise<string> => new Promise((resolve, reject) => {
-	console.log('Fetching', url.href);
+const get = async (url: URL): Promise<string> =>
+	new Promise((resolve, reject) => {
+		console.log('Fetching', url.href);
 
-	https.get(url, response => {
-		const result: Buffer[] = [];
+		https
+			.get(url, response => {
+				const result: Buffer[] = [];
 
-		response.on('data', chunk => {
-			result.push(chunk);
-		}).on('end', () => {
-			resolve(Buffer.concat(result).toString());
-		}).on('error', reject);
-	}).on('error', reject);
-});
+				response
+					.on('data', chunk => {
+						result.push(chunk);
+					})
+					.on('end', () => {
+						resolve(Buffer.concat(result).toString());
+					})
+					.on('error', reject);
+			})
+			.on('error', reject);
+	});
 
-const base = 'https://bitbucket.org/jvdl/correcthorsebatterystaple/raw/master/data/';
+const base
+	= 'https://bitbucket.org/jvdl/correcthorsebatterystaple/raw/master/data/';
 
-const words = new Set<string>();
+void (async (): Promise<void> => {
+	const words = new Set<string>();
 
-for (const filename of filenames) {
-	const url = new URL(`./${filename}.txt`, base);
+	for (const filename of filenames) {
+		const url = new URL(`./${filename}.txt`, base);
 
-	// eslint-disable-next-line no-await-in-loop
-	const rawWords = await get(url);
+		// eslint-disable-next-line no-await-in-loop
+		const rawWords = await get(url);
 
-	const splitWords = rawWords.split(',');
+		const splitWords = rawWords.split(',');
 
-	for (const word of splitWords) {
-		const sanitized = word.trim().toLowerCase();
+		for (const word of splitWords) {
+			const sanitized = word.trim().toLowerCase();
 
-		if (/^[a-z]{4,10}$/i.test(sanitized)) {
-			words.add(sanitized);
+			if (/^[a-z]{4,10}$/i.test(sanitized)) {
+				words.add(sanitized);
+			}
 		}
 	}
-}
 
-const collator = new Intl.Collator('en-US');
-const sortedWords = [...words].sort(collator.compare);
+	const collator = new Intl.Collator('en-US');
+	const sortedWords = [...words].sort(collator.compare);
 
-// This runs in #/dist/scripts/get-lingo-words.js
-const fileUrl = new URL('../../src/lingo/wordlist/words.json', import.meta.url);
-await fs.writeFile(fileUrl, JSON.stringify(sortedWords));
+	// This runs in #/dist/scripts/get-lingo-words.js
+	const fileUrl = path.resolve(
+		// eslint-disable-next-line unicorn/prefer-module
+		__dirname,
+		'../../src/lingo/wordlist/words.json',
+	);
+	await fs.writeFile(fileUrl, JSON.stringify(sortedWords));
+})();
