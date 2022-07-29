7a63935db6ff8609e8df7c5753fc64ececceb553
Luca <lusc.dev@gmail.com>
2022-07-29 20:25:06 +0200

    Extract keyboardless-input.tsx

--- src/sudoku/script.tsx
+++ src/sudoku/script.tsx
@@ -1,9 +1,10 @@
+import {Sudoku, type SubscriptionCallback} from '@lusc/sudoku';
 import React from 'react';
 import {createRoot} from 'react-dom/client.js';
-import {type SubscriptionCallback, Sudoku} from '@lusc/sudoku';
 
-import * as sudokuExamples from './sudoku-examples';
 import {Cell} from './components/cell';
+import {KeyboardlessInput} from './components/keyboardless-input';
+import * as sudokuExamples from './sudoku-examples';
 
 type StateCell = {
 	element: string | undefined;
@@ -30,19 +31,6 @@ interface AppState {
 	error: undefined | string;
 	focused: number;
 }
-
-const SvgEraser: React.FC = () => (
-	<svg
-		width='16'
-		height='16'
-		fill='currentColor'
-		className='svg-eraser-fill'
-		viewBox='0 0 16 16'
-	>
-		<path d='M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z' />
-	</svg>
-);
-
 class App extends React.Component<Record<string, unknown>, AppState> {
 	#sudoku = Sudoku.fromPrefilled(sudokuExamples.sudokuExpert, 9);
 
@@ -144,25 +132,7 @@ class App extends React.Component<Record<string, unknown>, AppState> {
 				>
 					Clear
 				</button>
-				<div className='keyboardless-inputs'>
-					{Array.from({length: 9}, (_v, index) => (
-						<div
-							key={index}
-							className='keyboardless-input'
-							title={`${index + 1}`}
-							onClick={this.handleKeyboardlessClick(`${index + 1}`)}
-						>
-							{index + 1}
-						</div>
-					))}
-					<div
-						className='keyboardless-input input-eraser'
-						title='Clear cell'
-						onClick={this.handleKeyboardlessClick(' ')}
-					>
-						<SvgEraser />
-					</div>
-				</div>
+				<KeyboardlessInput onClick={this.handleInput} />
 			</div>
 		);
 	}
@@ -294,10 +264,6 @@ class App extends React.Component<Record<string, unknown>, AppState> {
 
 			return {focused};
 		});
-	};
-
-	handleKeyboardlessClick = (number: string) => (): void => {
-		this.handleInput(number);
 	};
 
 	fromString = (input: string): void => {
