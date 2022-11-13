bc517dcd2ec21436a77823d45cbf74d1a6050b39
Luca <61409445+melusc@users.noreply.github.com>
2022-11-13 10:13:58 +0100

    Disable deprecated address.streetName

--- src/quick-faker/components/modules/address.tsx
+++ src/quick-faker/components/modules/address.tsx
@@ -25,7 +25,6 @@ export const Address: React.FC = () => (
 			'stateAbbr',
 			'street',
 			'streetAddress',
-			'streetName',
 			'timeZone',
 			'zipCode',
 
@@ -37,6 +36,7 @@ export const Address: React.FC = () => (
 			// 'citySuffix',
 			// 'streetPrefix',
 			// 'streetSuffix',
+			// 'streetName',
 		]}
 	/>
 );
