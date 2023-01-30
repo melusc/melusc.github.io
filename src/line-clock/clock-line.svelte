5078ad8c6ade2f24abe1b25944f06a59aee1ccd1
Luca <61409445+melusc@users.noreply.github.com>
2023-01-30 19:53:59 +0100

    Convert line-clock to svelte

---
+++ src/line-clock/clock-line.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let to: number;
+	export let active: number;
+</script>
+
+<div class="clock-row" style:--offset={active}>
+	{#each {length: to + 1} as _, index (index)}
+		<div class:active={active === index}>
+			{index}
+		</div>
+	{/each}
+</div>
