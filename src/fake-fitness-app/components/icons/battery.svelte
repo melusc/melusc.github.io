ff124bbf6acdc4c8f4c189123cd791a19bc9e00a
Luca <61409445+melusc@users.noreply.github.com>
2023-01-30 17:59:53 +0100

    Convert fake-fitness-app to svelte

---
+++ src/fake-fitness-app/components/icons/battery.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let batteryLevel: string = '100%';
+	$: batteryPercent = Number(/^\d+/.exec(batteryLevel)?.[0] ?? 0);
+</script>
+
+<svg
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	stroke-width="1.5"
+	stroke="currentColor"
+	viewBox="0 0 24 24"
+	transform="rotate(-90)"
+	class="icon-battery"
+	class:invalid={batteryPercent <= 15}
+>
+	<path
+		d="M6 7h11a2 2 0 0 1 2 2v.5a.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5v.5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2"
+	/>
+	{#if batteryPercent > 0}<path d="M7 10v4" />{/if}
+	{#if batteryPercent > 25}<path d="M10 10v4" />{/if}
+	{#if batteryPercent > 50}<path d="M13 10v4" />{/if}
+	{#if batteryPercent > 75}<path d="M16 10v4" />{/if}
+</svg>
--- src/fake-fitness-app/components/icons/bluetooth.svelte
+++ src/fake-fitness-app/components/icons/bluetooth.svelte
@@ -7,7 +7,7 @@
 	stroke-linejoin="round"
 	stroke="currentColor"
 >
-  <polyline points="7 8 17 16 12 20 12 4 17 8 7 16" />
-  <line x1="4" y1="12" x2="5" y2="12" />
-  <line x1="18" y1="12" x2="19" y2="12" />
+	<polyline points="7 8 17 16 12 20 12 4 17 8 7 16" />
+	<line x1="4" y1="12" x2="5" y2="12" />
+	<line x1="18" y1="12" x2="19" y2="12" />
 </svg>
