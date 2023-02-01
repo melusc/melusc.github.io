90ad401832f0736f9d5ddee0d9f9620a702f665c
Luca <61409445+melusc@users.noreply.github.com>
2023-02-01 21:56:57 +0100

    Add eslint svelte (#386)

--- src/sudoku/main.ts
+++ src/sudoku/main.ts
@@ -1,6 +1,5 @@
 import App from './App.svelte';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const app = new App({
 	target: document.querySelector('#app')!,
 });
