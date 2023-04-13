fae2fa4b7fd1ec9af3e727a1704271f69061479a
Luca <61409445+melusc@users.noreply.github.com>
2023-04-13 09:26:13 +0200

    Use querySelector to find nodes

--- src/unlock-office-documents/unlock.ts
+++ src/unlock-office-documents/unlock.ts
@@ -20,21 +20,17 @@ export async function unlock(file: File): Promise<Blob> {
 
 			const text = await file.async('text');
 			if (
-				!/sheetprotection|workbookprotection|w:documentprotection/i.test(text)
+				!/sheetprotection|workbookprotection|documentprotection/i.test(text)
 			) {
 				return;
 			}
 
 			const dom = parser.parseFromString(text, 'text/xml');
-			for (const tag of [
-				'sheetProtection',
-				'workbookProtection',
-				'w:documentProtection',
-			]) {
-				// eslint-disable-next-line unicorn/prefer-query-selector
-				for (const element of dom.getElementsByTagName(tag)) {
-					element.remove();
-				}
+
+			for (const element of dom.querySelectorAll(
+				'sheetProtection, workbookProtection, documentProtection',
+			)) {
+				element.remove();
 			}
 
 			zip.file(name, serializer.serializeToString(dom));
