be5c77512b5ae1eb9fc8308e0cbcbf41a1de5f57
Luca <61409445+melusc@users.noreply.github.com>
2023-04-17 18:11:01 +0200

    Add unlock-office-documents

---
+++ src/routes/unlock-office-documents/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import Download from './download.svelte';
+	import {unlock} from './unlock';
+	import Upload from './upload.svelte';
+	import './style.scss';
+
+	let name: string;
+
+	let output: Promise<Blob> | undefined;
+
+	function handleUpload(event: CustomEvent<{name: string; file: File}>): void {
+		output = unlock(event.detail.file);
+		name = event.detail.name;
+	}
+</script>
+
+<svelte:head>
+	<title>Unlock Office Documents</title>
+</svelte:head>
+
+<Upload on:input={handleUpload} />
+
+{#if output}
+	{#await output then file}
+		<Download {file} {name} />
+	{:catch error}
+		<div class="error">
+			{error instanceof Error ? error.message : error}
+		</div>
+	{/await}
+{/if}
+
+<div class="report-issue">
+	Not working as expected?
+	<a
+		href="https://github.com/melusc/melusc.github.io/issues"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		Report an issue
+	</a>
+</div>
+
+<style>
+	.error {
+		margin-top: 2em;
+		font-size: 1.4em;
+		color: var(--red);
+	}
+
+	.report-issue {
+		position: absolute;
+		right: 2em;
+		bottom: 2em;
+	}
+</style>
---
+++ src/routes/unlock-office-documents/unlock.ts
@@ -0,0 +1,41 @@
+import {loadAsync} from 'jszip';
+import type JSZip from 'jszip';
+
+export async function unlock(file: File): Promise<Blob> {
+	let zip: JSZip;
+	try {
+		zip = await loadAsync(file);
+	} catch {
+		throw new Error('Could not open file as zip');
+	}
+
+	const parser = new DOMParser();
+	const serializer = new XMLSerializer();
+
+	await Promise.all(
+		Object.entries(zip.files).map(async ([name, file]) => {
+			if (!name.endsWith('.xml')) {
+				return;
+			}
+
+			const text = await file.async('text');
+			if (
+				!/sheetprotection|workbookprotection|documentprotection/i.test(text)
+			) {
+				return;
+			}
+
+			const dom = parser.parseFromString(text, 'text/xml');
+
+			for (const element of dom.querySelectorAll(
+				'sheetProtection, workbookProtection, documentProtection',
+			)) {
+				element.remove();
+			}
+
+			zip.file(name, serializer.serializeToString(dom));
+		}),
+	);
+
+	return zip.generateAsync({type: 'blob'});
+}
