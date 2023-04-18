0a28a28f9eb19a9fc84eaa85b2bbc9abfc145ab8
Luca <61409445+melusc@users.noreply.github.com>
2023-04-18 12:04:21 +0200

    animated-triangle: Move most of css to svelte file

--- src/routes/animated-triangle/+page.svelte
+++ src/routes/animated-triangle/+page.svelte
@@ -9,3 +9,35 @@
 <svg fill="hsl(0, 100%, 70%)" viewBox="0 0 256 256">
 	<path d="M10 10L246 246H10z" />
 </svg>
+
+<style lang="scss">
+	@use 'sass:math';
+
+	svg {
+		width: 100vmin;
+		height: 100vmin;
+		animation: colour 4s infinite alternate linear;
+	}
+
+	path {
+		animation: path 4s infinite alternate linear;
+	}
+
+	@keyframes path {
+		0%,
+		100% {
+			d: path('M10 10L246 246H10z');
+		}
+		50% {
+			d: path('M246 10L246 246H10z');
+		}
+	}
+
+	@keyframes colour {
+		@for $i from 0 through 360 {
+			#{math.div($i, 3.6) * 1%} {
+				fill: hsl($i, 100%, 70%);
+			}
+		}
+	}
+</style>
