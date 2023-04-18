abead8b4ab290a81bb95dfef7732c56293a61990
Luca <61409445+melusc@users.noreply.github.com>
2023-04-18 10:58:38 +0200

    Add animated-triangle

---
+++ src/routes/animated-triangle/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import './style.scss';
+</script>
+
+<svelte:head>
+	<title>Animated triangle</title>
+</svelte:head>
+
+<svg fill="hsl(0, 100%, 70%)" viewBox="0 0 256 256">
+	<path d="M10 10L246 246H10z" />
+</svg>
