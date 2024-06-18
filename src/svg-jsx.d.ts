ccbeeb8b055321caaf1fe43ff7bc538a61e5c2e9
Luca <61409445+melusc@users.noreply.github.com>
2024-06-18 08:48:35 +0200

    Remove unused svg-jsx.d.ts

--- src/svg-jsx.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg' {
-	import type React from 'react';
-
-	export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
-}
-
-declare module '*.svg?url' {
-	export default string;
-}
